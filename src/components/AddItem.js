c8da540bc39e6e386a0ba15275bb7ee2eb97c051
Yaseen PK <136886058+yaseenpkay@users.noreply.github.com>
2024-12-24 15:15:39 +0530

    Update AddItem.js

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -132,7 +132,7 @@ function AddItem({ addTask, onClose, sections }) {
                 type="button"
                 className="px-4 py-2 bg-gray-300 text-white rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
               >
-                Cancel
+                Cance
               </button>
               <button
                 type="submit"
